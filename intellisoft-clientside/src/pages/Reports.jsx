758a93560bb6aef7c18fd2e32f41b2675cfd1884
Nelson Kimaiga <nelson.guto@jubileekenya.com>
2023-04-09 08:34:39 +0300

    Add filtering options, code recfactoring

--- intellisoft-clientside/src/pages/Reports.jsx
+++ intellisoft-clientside/src/pages/Reports.jsx
@@ -3,6 +3,7 @@ import 'jquery/dist/jquery.min.js';
 import "datatables.net-dt/js/dataTables.dataTables";
 import "datatables.net-dt/css/jquery.dataTables.min.css";
 import $ from 'jquery';
+import Moment from 'react-moment';
 
 class Reports extends React.Component {
     constructor(props) {
@@ -23,6 +24,8 @@ class Reports extends React.Component {
             }
         };
 
+        // Fetch the visits from the database based on the patient's number.
+
         const response = await fetch(`http://localhost:8008/intellisoft/visits/${patientNumber}`, requestOptions);
 
         const data = await response.json();
@@ -33,7 +36,7 @@ class Reports extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if (prevState.data !== this.state.data) {
+        if (prevState.data !== this.state.data || prevState.filteredData !== this.state.filteredData) {
             var table = $('#patientTable').DataTable({
                 "iDisplayLength": 25,
                 pagingType: "simple"
@@ -48,32 +51,68 @@ class Reports extends React.Component {
         }
     }
 
+
+    handleDateFilter = (event) => {
+        const date = new Date(event.target.value);
+        const filteredData = this.state.data.filter((visit) => {
+            const visitDate = new Date(visit.visitDate);
+            return visitDate.toDateString() === date.toDateString();
+        });
+        this.setState({ filteredData: filteredData });
+    }
+    
+
+
     render() {
         return (
             this.state.data !== null ? (
                 <React.Fragment>
                     <div className="container" style={{ marginTop: 10 }}>
                         <div className="row justify-content-md-center">
-                            <h3>Patient Report</h3>
+                            <h3 class="text-center">Patient Report</h3>
+                            <div className="row justify-content-end">
+                                <div className='col-lg-4'>
+                                    <label htmlFor="dateFilter">Filter by Date: </label>
+                                    <input type="date" className="form-control" id="dateFilter" onChange={this.handleDateFilter} />
+                                </div>
+                            </div>
+                            <br />
+
                             <table id="patientTable" className="table table-striped table-bordered table-sm hover mb-2">
                                 <thead>
                                     <tr>
                                         <td>Full Names</td>
                                         <td>Age</td>
                                         <td>BMI Status</td>
+                                        <td>Visit Date</td>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {this.state.data.map((visit, index) => {
-                                        return (
-                                            <tr key={index}>
-                                                <td>{visit.patientName}</td>
-                                                <td>{visit.age}</td>
-                                                <td>{visit.bmiStatus}</td>
-                                            </tr>
-                                        )
-                                    })}
+                                    {this.state.filteredData ? (
+                                        this.state.filteredData.map((visit, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{visit.patientName}</td>
+                                                    <td>{visit.age}</td>
+                                                    <td>{visit.bmiStatus}</td>
+                                                    <td><Moment format="YYYY-MM-DD">{visit.visitDate}</Moment></td>
+                                                </tr>
+                                            )
+                                        })
+                                    ) : (
+                                        this.state.data.map((visit, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{visit.patientName}</td>
+                                                    <td>{visit.age}</td>
+                                                    <td>{visit.bmiStatus}</td>
+                                                    <td><Moment format="YYYY-MM-DD">{visit.visitDate}</Moment></td>
+                                                </tr>
+                                            )
+                                        })
+                                    )}
                                 </tbody>
+
                             </table>
                         </div>
                     </div>
